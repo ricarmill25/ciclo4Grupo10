afb966d57fb7efb4e87af2f2a27102702e8574ff
ricarmill25 <90973983+ricarmill25@users.noreply.github.com>
2021-11-28 01:32:25 -0500

    Delete src directory

--- src/Components/Btn/BtnCerrarSesion.jsx
+++
@@ -1,8 +0,0 @@
-const BtnCerrarSesion = () => {
-    return (  
- <div>
-    <a className="btn btn-warning" aria-current="page"  href="/">Cerrar Sesión</a>
-</div>
-);
-}
-export default BtnCerrarSesion;
--- src/Components/Btn/BtnRegresarMenu.jsx
+++
@@ -1,8 +0,0 @@
-function BtnRegresarMenu (link) {
-    return (  
-    <li className="nav-item">
-        <a className="nav-link" aria-current="page"  href={link.page}>Menú de opciones</a>
-    </li>
-);
-}
-export default BtnRegresarMenu;
